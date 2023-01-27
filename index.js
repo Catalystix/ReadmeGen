0ae9e9a5c76869080f068c5a3b61ed94cdce257d
Catalystix <dcode.green@gmail.com>
2023-01-26 20:56:46 -0500

    added prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
 // TODO: Include packages needed for this application
+const genMd = require('./utils/generateMarkdown');
 
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const licenseQuestions = [];
+const
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(inquirer) {
+    // run prompts
+    const inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'What was your motivation?',
+                name: 'input',
+            },
+            {
+                type: 'input',
+                message: 'Why did you build this project?',
+                name: 'input',
+            },
+            {
+                type: 'input',
+                message: 'What problem does it solve?',
+                name: 'input',
+            },
+            {
+                type: 'input',
+                message: 'What did you learn?',
+                name: 'input',
+            },
+            {
+                type: 'input',
+                message: 'What makes your project stand out?',
+                name: 'input',
+            },
+            {
+                type: 'checkbox',
+                message: 'License Type',
+                name: 'License Type',
+                choices: ['MIT', 'GPLv3', 'GPL'],
+            },
+            {
+                type: 'list',
+                message: 'What is your preferred method of communication?',
+                name: 'Comm-method',
+                choices: ['email', 'call', 'text']
+            },
+            {
+                type: 'input',
+                message: 'What is your preferred method of communication?',
+                name: 'confirm',
+            },
+
+        ])
+        .then((response) => {
+            writeToFile('README.md', response);
+            console.log('Thanks for your time!')
+        });
+};
+
+// TODO: Create a function to write README file
+fs.writeFile('README.md', JSON.stringify(response), function (err) {
+    err ? console.log('error', err) : console.log('success');
+});
+function writeToFile(fileNaREAme, data) {
+    fs.appendFile('README.md', data, (err) =>
+        err ? console.error(err) : console.log('add logged'))
+}
+
+// TODO: Create a function to initialize app
+function init(inquirer) {
+    // run prompts
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license;
+  let yourLicense = '';
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  } else {
+    license= "N/A"
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
